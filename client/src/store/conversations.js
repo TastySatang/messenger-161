1e81d309773d6dfb1ddec785d1e9e9ddb9bc8973
junyeoble <junyeoble@gmail.com>
2021-11-03 17:05:31 -0700

    everything working

--- client/src/store/conversations.js
+++ client/src/store/conversations.js
@@ -69,7 +69,7 @@ export const addConversation = (recipientId, newMessage) => {
 
 // REDUCER
 
-const reducer = (state = {}, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case GET_CONVERSATIONS:
       return action.conversations;
