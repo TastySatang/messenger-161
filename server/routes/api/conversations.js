a317cf8cf9e05b631ba04efe6e675bed7fb53101
junyeoble <junyeoble@gmail.com>
2021-11-04 23:03:18 -0700

    Manually sort conversations to correct order

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -21,7 +21,9 @@ router.get("/", async (req, res, next) => {
       attributes: ["id"],
       order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "ASC"] },
+        {
+          model: Message,
+        },
         {
           model: User,
           as: "user1",
@@ -68,11 +70,13 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
+      convoJSON.latestMessageTime = convoJSON.messages[convoJSON.messages.length - 1].createdAt;
       conversations[i] = convoJSON;
     }
 
-    res.json(conversations);
+    conversations.sort((a, b) => b.latestMessageTime - a.latestMessageTime)
+    await res.json(conversations);
   } catch (error) {
     next(error);
   }
